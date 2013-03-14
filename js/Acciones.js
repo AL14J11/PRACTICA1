c40b6e89dca3edff31d63c51c04cd259b09bf989
BRYAN <nabyrDR@gmail.com>
2013-03-14 08:08:44 -0600

    1

---
+++ js/Acciones.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+$(document).ready(  function(e){
+ //alert ("LISTO!!!")
+ document.addEventListener("deviceready", function() {
+	 $(' .btn').tap(function() {
+		 var boton = (($(this).attr('class')).split
+		 (' '))[1];
+		 switch(boton)
+		{
+			case 'b1':
+				navigator.notification.beep(1);
+			break;
+			case 'b2':
+				navigator.notification.beep(2);
+			break;
+			case 'b3':
+				navigator.notification.beep(3);
+			break;
+			case 'v1':
+				navigator.notification.vibrate(500);
+			break;
+			case 'v2':
+				navigator.notification.vibrate(500);
+				navigator.notification.vibrate(1000);
+			break;
+		}
+	 });
+ },false);	
+});
