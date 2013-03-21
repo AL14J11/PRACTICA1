a129d81b7554534d8bca30d8c957b0cb66e26164
AL14J11 <nabyrDR@gmail.com>
2013-03-21 08:06:57 -0600

    Acciones1.1

--- js/Acciones.js
+++ js/Acciones.js
@@ -1,30 +1,45 @@
 // JavaScript Document
-$(document).ready(  function(e){
- //alert ("LISTO!!!")
- document.addEventListener("deviceready", function() {
-	 $('.btn').tap(function() {
-		 var boton = (($(this).attr('class')).split(' '))[1];
-		 switch(boton)
-		{
-			alert(boton);
-			case 'b1':
-				navigator.notification.beep(1);
-			break;
-			case 'b2':
-				navigator.notification.beep(2);
-			break;
-			case 'b3':
-				navigator.notification.beep(3);
-			break;
-			case 'v1':
-				navigator.notification.vibrate(500);
-			break;
-			case 'v2':
-				navigator.notification.vibrate(500);
-				navigator.notification.vibrate(1000);
-			break;
-		}
-	 });
- },false);	
+$(document).ready(function(e) {
+	//alert("Listo")
+	document.addEventListener("deviceready",function(){
+		$('.btn').tap(function(){
+			var boton= (($(this).attr('class')).split(' '))[1];
 
+//			alert(boton)
+			switch(boton)
+			{
+				case 'b1':
+				navigator.notification.beep(1);
+				break;
+				case 'b2':
+				navigator.notification.beep(2);
+				break;
+				case 'b3':
+				navigator.notification.beep(5);
+				break;
+				case 'v1':
+				navigator.notification.vibrate(500);
+				break;
+				case 'v2':
+				navigator.notification.vibrate(500);
+				navigator.notification.vibrate(500);
+				break;
+			}
+			$("#derecha").swiperight(function(){
+				navigator.notification.alert("Desliz&oacute; a la derecha",function(){"Practica 1", "Aceptar"});
+			});
+		   $("#izquierda").swipeleft(function(){
+			  navigator.notification.confirm('¿Qué desea hacer?', function(opcion){
+				  switch(opcion){
+					  case 1:
+					  navigator.notification.beep(1);
+					  break;
+					  case 2:
+					  navigator.notification.vibrate(500);
+					  break;
+				  }//switch
+			  },"Práctica 1","Beep,Vibrar,Cancelar");//function confirm
+		   });//function swipe
+	});
+	},false);
 });
